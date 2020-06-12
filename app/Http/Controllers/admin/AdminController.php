f37ad35e317e644614cd765452d661a0b0a9ec09
Ningaanza Remy <remimaradona@gmail.com>
2020-06-12 02:57:56 -0400

    Created Controller for Admin and for statics pages

---
+++ app/Http/Controllers/admin/AdminController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function dashboard(){
+        return view('admin/dashboard');
+    }
+    public function create(){
+        return view('admin/food-categories/create');
+    }
+}
